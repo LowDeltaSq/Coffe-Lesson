15399238dfc09649f76d1faa6de6d85309b4bcbd
Stormxx <fazilovadil658111@gmail.com>
2024-10-12 03:18:50 +0500

    sec

--- src/components/App/Page6.tsx
+++ src/components/App/Page6.tsx
@@ -2,7 +2,9 @@ export default function Page6() {
   return (
     <div className="text-[#c4d702] bg-[url('/bg.png')] bg-cover h-screen flex flex-col justify-center items-center pt-36 md:pt-0">
       <div className="flex justify-center pb-20 md:pb-52">
-        <p className="text-3xl md:text-6xl font-bold b">TESTIMONIALS</p>
+        <p className="text-3xl md:text-6xl font-bold b pt-20 md:pt-0">
+          TESTIMONIALS
+        </p>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 mx-5 md:mx-20 gap-10 md:gap-20 text-center">
@@ -35,7 +37,7 @@ export default function Page6() {
         </div>
         <div className="grid gap-8 pb-20 md:pb-0">
           <p className="text-xl md:text-4xl font-semibold b">DIANA CRUZ</p>
-          <p className="text-sm md:text-lg">
+          <p className="text-sm md:text-lg pb-20 md:pb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores qui
             suscipit, aspernatur odit, animi hic dignissimos eos magni deserunt,
             laboriosam iste libero quas iusto? Laborum eos nesciunt deleniti
